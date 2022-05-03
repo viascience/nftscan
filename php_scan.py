d10dc0889b6ad48da78db146ac571e39fa0c67e8
jcardenes <jcardenes@viascience.com>
2022-05-03 09:38:48 -0400

    php and javascript scan

---
+++ php_scan.py
@@ -0,0 +1,13 @@
+import binascii
+
+filename = "test_JackSparrow"
+filename = "test.php.jpg"
+
+with open(filename, 'rb') as f: 
+    content = f.read() 
+    if binascii.hexlify(b'__halt') in binascii.hexlify(content): 
+        print('PHP found') 
+    
+    if binascii.hexlify(b'/*') in binascii.hexlify(content) and binascii.hexlify(b'*/') in binas
+cii.hexlify(content): 
+        print('JS found')
